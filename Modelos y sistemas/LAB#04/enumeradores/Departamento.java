48a89a31d23cfe24b42ad4ee01ad5a6c2d4afa4e
Tiziano Larocca <laroccatiziano1@gmail.com>
2026-05-03 20:17:21 -0300

    Add files via upload

---
+++ Modelos y sistemas/LAB#04/enumeradores/Departamento.java
@@ -0,0 +1,5 @@
+package enumeradores;
+
+public enum Departamento {
+    SISTEMAS, RRHH, VENTAS, FINANZAS;
+}
---
+++ Modelos y sistemas/LAB#04/enumeradores/TipoContrato.java
@@ -0,0 +1,5 @@
+package enumeradores;
+
+public enum TipoContrato {
+    FULLTIME, PARTTIME, CONTRATADO;
+}
