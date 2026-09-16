efe19cea3c99262b0176ce4c06e9e7b57f641849
Tiziano Larocca <laroccatiziano1@gmail.com>
2026-09-15 23:14:46 -0300

    Create ProductRepository.java

---
+++ Proyecto, diseño e implementación de sistemas computacionales/LAB#API-REST-V2/API-REST-V2/back/product/src/main/java/com/api/product/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.api.product.repository;
+
+import com.api.product.dto.ProductDTO;
+import com.api.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}
